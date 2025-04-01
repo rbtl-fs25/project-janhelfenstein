4eda925e7ddad95e5398757355502bdd88fdb8e7
janhelfenstein <jan_marvin@hotmail.com>
2025-04-01 09:05:07 +0000

    created a readme md file. completed data download script. added intro comments to data cleaning script.

--- R/01-data-download.R
+++ R/01-data-download.R
@@ -1,1 +1,15 @@
 # 01-data-download.R
+# This script downloads data from the google survey about waste separation,
+# which we did in the RBTL course in FS25
+# Author: Jan Helfenstein
+# Date: 2025-04-01
+
+# libraries
+library(googlesheets4)
+library(tidyverse)
+
+# import data from google sheet
+survey_raw <- read_sheet("https://docs.google.com/spreadsheets/d/1GKnJ1hurZ5QffM6urSp2GiSltdq1ep2GtML-aTGM6v0/edit?usp=sharing")
+
+# store data
+write_csv(survey_raw, "data/raw/survey-raw.csv")
--- R/02-data-cleaning.R
+++ R/02-data-cleaning.R
@@ -1,1 +1,7 @@
 # 02-data-cleaning.R
+# This script cleans and tidies the data from the google survey about waste separation,
+# which we did in the RBTL course in FS25
+# Author: Jan Helfenstein
+# Date: 2025-04-01
+
+# I already did this in an earlier assignment.
